a30d7909fa9dc59195c975988a0fdc04bac76720
neverless6661 <lsca_lubani@hotmail.com>
2022-07-26 20:05:39 -0500

    modificaciones

--- app/src/main/java/com/example/textexercise1/MainActivity.java
+++ app/src/main/java/com/example/textexercise1/MainActivity.java
@@ -3,8 +3,11 @@ package com.example.textexercise1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnNew;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
