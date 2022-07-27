385a8dc8a66533ce29bab3921ee1bafa413feccd
neverless6661 <lsca_lubani@hotmail.com>
2022-07-27 09:48:00 -0500

    modificacion main

--- app/src/main/java/com/example/textexercise1/MainActivity.java
+++ app/src/main/java/com/example/textexercise1/MainActivity.java
@@ -14,5 +14,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnNew = (Button) findViewById(R.id.btnNew);
     }
 }
